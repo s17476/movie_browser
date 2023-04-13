47e39cb4baa36d76b5d4242a86a3d1f3397a2d1c
Grzegorz Fraczek <gf@lnasoftware.com>
2023-04-13 19:29:12 +0200

    Firebase core + auth added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:movie_browser/injection.dart';
 
 import 'app/movie_browser.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   configureDependencies();
